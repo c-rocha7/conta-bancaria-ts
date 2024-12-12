d39851e0369066691ced626e9b9cff09b341ee0b
Cauã R. Pereira <7aauac@gmail.com>
2024-12-12 15:20:54 -0300

    feat: created class ContaCorrente

---
+++ src/model/ContaCorrente.ts
@@ -0,0 +1,40 @@
+import { Conta } from './Conta';
+
+export class ContaCorrente extends Conta {
+  private _limite: number;
+
+  constructor(
+    numero: number,
+    agencia: number,
+    tipo: number,
+    titular: string,
+    saldo: number,
+    limite: number
+  ) {
+    super(numero, agencia, tipo, titular, saldo);
+    this._limite = limite;
+  }
+
+  public get limite(): number {
+    return this._limite;
+  }
+
+  public set limite(value: number) {
+    this._limite = value;
+  }
+
+  public visualizar(): void {
+    super.visualizar();
+    console.log(`Limite da Conta: ${this._limite}`);
+  }
+
+  public sacar(valor: number): boolean {
+    if (this.saldo + this._limite < valor) {
+      console.log('\nSaldo Insuficiente!');
+      return false;
+    }
+
+    this.saldo -= valor;
+    return true;
+  }
+}
